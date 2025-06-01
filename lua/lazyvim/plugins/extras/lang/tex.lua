c29ac7a1cf37d1c37c181b17b387bcd019fb6fae
Juan Rodrigo <juanrodr100@student.kau.se>
2025-06-01 21:20:56 +0200

    reverted minipairs

--- lua/lazyvim/plugins/extras/lang/tex.lua
+++ lua/lazyvim/plugins/extras/lang/tex.lua
@@ -22,17 +22,17 @@ return {
     end,
   },
 
-  {
-    "lervag/vimtex",
-    lazy = false, -- lazy-loading will disable inverse search
-    config = function()
-      vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
-      vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
-    end,
-    keys = {
-      { "<localLeader>l", "", desc = "+vimtex" },
-    },
-  },
+  -- {
+  --   "lervag/vimtex",
+  --   lazy = false, -- lazy-loading will disable inverse search
+  --   config = function()
+  --     vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
+  --     vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+  --   end,
+  --   keys = {
+  --     { "<localLeader>l", "", desc = "+vimtex" },
+  --   },
+  -- },
 
   -- Correctly setup lspconfig for LaTeX 🚀
   {
